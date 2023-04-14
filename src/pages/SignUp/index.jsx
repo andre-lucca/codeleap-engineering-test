c67d8dde0a14fa1b2400a925e7684412773825bc
Andre Luca <lucaprojob@gmail.com>
2023-04-14 10:14:31 -0300

    add save username on localStorage functionality

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import style from './style.module.css'
 
 export function SignUp() {
   const [usernameInputValue, setUsernameInputValue] = useState('')
-  const isEnterButtonDisabled = !Boolean(usernameInputValue)
+  const isEnterButtonDisabled = usernameInputValue.replaceAll(' ', '') === ''
+  const navigateTo = useNavigate()
 
   const handleUsernameInputChange = (inputValue) => {
     setUsernameInputValue(inputValue)
@@ -12,13 +14,18 @@ export function SignUp() {
 
   const handleEnterClick = (event) => {
     event.preventDefault()
+
+    localStorage.setItem('username', usernameInputValue.replaceAll(' ', ''))
+    navigateTo('/home')
   }
+
+  const warnEmptyInput = () => (isEnterButtonDisabled ? style.warnLabel : '')
 
   return (
     <div className={style.signUpContainer}>
       <form className={style.signUpForm} onSubmit={handleEnterClick}>
         <h2>Welcome to CodeLeap network!</h2>
-        <label htmlFor="username">Please enter your username</label>
+        <label className={warnEmptyInput()} htmlFor="username">Please enter your username</label>
         <input
           id="username"
           type="text"
@@ -26,7 +33,12 @@ export function SignUp() {
           value={usernameInputValue}
           onChange={({ target }) => handleUsernameInputChange(target.value)}
         />
-        <button type="submit" disabled={isEnterButtonDisabled}>Enter</button>
+        <button
+          type="submit"
+          disabled={isEnterButtonDisabled}
+        >
+          Enter
+        </button>
       </form>
     </div>
   )
