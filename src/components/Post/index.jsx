4d41c1ead8f489a3c253574c4d3bb9f92f26beed
Andre Luca <lucaprojob@gmail.com>
2023-04-14 10:16:11 -0300

    add conditional style of postOptions

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -4,26 +4,34 @@ import editImg from '../../assets/edit.svg'
 import style from './style.module.css'
 
 export function Post({ postData }) {
+  const postUsername = postData.username.replaceAll(' ', '')
+  const localUserName = localStorage.getItem('username')
+  const formattedUsername = postUsername.includes('@') ? postUsername : `@${postUsername}`
+
   return (
     <li className={style.postItem}>
       <section className={style.postHeader}>
         <h4 className={style.postTitle}>
           {postData.title}
         </h4>
-        <div className={style.postOptions}>
-          <img
-            src={deleteImg}
-            alt="Delete this post."
-          />
-          <img src={editImg}
-            alt="Edit this post."
-          />
-        </div>
+        {
+          localUserName === postUsername && (
+            <div className={style.postOptions}>
+              <img
+                src={deleteImg}
+                alt="Delete this post."
+              />
+              <img src={editImg}
+                alt="Edit this post."
+              />
+            </div>
+          )
+        }
       </section>
       <section className={style.postBody}>
         <div className={style.postInfo}>
           <span className={style.username}>
-            @{postData.username.trim().replace(' ', '')}
+            {formattedUsername}
           </span>
           <span className={style.howLongAgo}>
             25 minutos atrás
